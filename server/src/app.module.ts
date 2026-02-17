c356802651f30953af2e7f3107fe7473347d12ba
psu6810110485 <6810110485@psu.ac.th>
2026-02-17 23:21:30 +0700

    (backend):connect NestJS to SQL database via TypeORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,22 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // หรือ 'mysql' ตามที่คุณตั้งไว้ใน Docker
+      host: 'localhost',
+      port: 5432,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'nexus_gear_db', // ชื่อโปรเจกต์ของคุณ
+      entities: [User],
+      synchronize: true, // เฉพาะช่วงพัฒนา: ให้ระบบสร้างตารางใหม่ตาม Entity อัตโนมัติ
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}

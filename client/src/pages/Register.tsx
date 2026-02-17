9181414b0e91c70e586307aef2e395fe529cc808
psu6810110485 <6810110485@psu.ac.th>
2026-02-17 15:04:49 +0700

    feat: setup router and register page structure

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Register.css'; // เชื่อมไฟล์ CSS ไว้รอเลย
+
+const Register = () => {
+  return (
+    <div className="register-container">
+      <h1>Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;

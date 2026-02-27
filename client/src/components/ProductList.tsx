64d2e73512cb0fc6e96f05cbb68cc2b38ecc316d
psu6810110455 <6810110455@psu.ac.th>
2026-02-27 18:30:33 +0700

    fix structure before coding

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import logoImg from '../assets/logo.png'; 
-
+//test1
 interface Product {
   id: number;
   name: string;

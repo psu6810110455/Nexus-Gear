cbdd9718e719580df67eaaa18d04c6d899c6dc76
psu6810110570 <chindanai.ac@gmail.com>
2026-03-11 02:39:24 +0700

    feat(products): เพิ่ม OrderItem ใน ProductsModule เพื่อรองรับดึงรีวิวสินค้า

--- server/src/products/products.module.ts
+++ server/src/products/products.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Category } from './entities/category.entity';  
+import { OrderItem } from '../orders/entities/order-item.entity';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Category])],
+  imports: [TypeOrmModule.forFeature([Product, Category, OrderItem])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
